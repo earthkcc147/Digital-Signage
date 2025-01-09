fcf299a4394016c0639564edb67a1bd16a6541f1
earthkcc147 <145793741+earthkcc147@users.noreply.github.com>
2025-01-09 15:02:36 +0700

    Update main.py

--- main.py
+++ main.py
@@ -69,6 +69,22 @@ def select_file():
     except Exception as e:
         print(f"เกิดข้อผิดพลาด: {e}")
 
+# ฟังก์ชันแสดงรายชื่อไฟล์ในโฟลเดอร์ check
+def list_files_in_folder():
+    # ตรวจสอบและสร้างโฟลเดอร์ 'check' ถ้ายังไม่มี
+    if not os.path.exists(FOLDER_NAME):
+        os.makedirs(FOLDER_NAME)
+        print(f"สร้างโฟลเดอร์ {FOLDER_NAME} เรียบร้อยแล้ว")
+    
+    print("\n--- ไฟล์ที่มีอยู่ในโฟลเดอร์ 'check' ---")
+    files = os.listdir(FOLDER_NAME)
+    if files:
+        for idx, file in enumerate(files, 1):
+            print(f"{idx}. {file}")
+    else:
+        print("ยังไม่มีไฟล์ในโฟลเดอร์")
+
+
 
 # ฟังก์ชันเพิ่มข้อมูลใหม่
 def add_data(file_path):
@@ -142,22 +158,6 @@ def display_data(file_path):
                 print("ยังไม่มีข้อมูลในระบบ")
     else:
         print("ยังไม่มีข้อมูลในระบบ")
-
-# ฟังก์ชันแสดงรายชื่อไฟล์ในโฟลเดอร์ check
-def list_files_in_folder():
-    # ตรวจสอบและสร้างโฟลเดอร์ 'check' ถ้ายังไม่มี
-    if not os.path.exists(FOLDER_NAME):
-        os.makedirs(FOLDER_NAME)
-        print(f"สร้างโฟลเดอร์ {FOLDER_NAME} เรียบร้อยแล้ว")
-    
-    print("\n--- ไฟล์ที่มีอยู่ในโฟลเดอร์ 'check' ---")
-    files = os.listdir(FOLDER_NAME)
-    if files:
-        for idx, file in enumerate(files, 1):
-            print(f"{idx}. {file}")
-    else:
-        print("ยังไม่มีไฟล์ในโฟลเดอร์")
-
 
 
 # ฟังก์ชันแก้ไขข้อมูล
