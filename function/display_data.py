b5e01235d8be706e8ad663e60e087e7c4649d0cb
earthkcc147 <145793741+earthkcc147@users.noreply.github.com>
2025-01-09 14:49:06 +0700

    Update display_data.py

--- function/display_data.py
+++ function/display_data.py
@@ -1,1 +1,22 @@
-...
+# ฟังก์ชันแสดงข้อมูลทั้งหมด
+def display_data(file_path):
+    if os.path.exists(file_path):
+        with open(file_path, 'r', encoding='utf-8') as file:
+            data = json.load(file)
+            if data:
+                print("\n{:<5} {:<20} {:<15} {:<30} {:<20}".format(
+                    "ลำดับ", "รายการ", "S/N", "อาการ", "วันที่และเวลาที่ตรวจ"
+                ))
+                print("-" * 90)
+                for entry in data:
+                    print("{:<5} {:<20} {:<15} {:<30} {:<20}".format(
+                        entry["ลำดับ"],
+                        entry["รายการ"],
+                        entry["s/n"],
+                        entry["อาการ"],
+                        entry["วันที่และเวลาที่ตรวจ"]
+                    ))
+            else:
+                print("ยังไม่มีข้อมูลในระบบ")
+    else:
+        print("ยังไม่มีข้อมูลในระบบ")
