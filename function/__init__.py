8429d3f03b6b5f94f6a23cbfb1a9c1ea4ee6366c
earthkcc147 <145793741+earthkcc147@users.noreply.github.com>
2025-01-10 12:23:06 +0700

    Update __init__.py

--- function/__init__.py
+++ function/__init__.py
@@ -1,5 +1,5 @@
 from .all_2 import initialize_file, list_files_in_folder, select_file, add_data, display_data, delete_data, edit_data, search_data, explain_program
 
-from .color import print_header, print_menu, print_alarm, print_error
+from .colour import print_header, print_menu, print_alarm, print_error
 
 
