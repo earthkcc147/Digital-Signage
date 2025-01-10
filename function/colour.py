7e9e5f6fd1d214545ec20fb9bf376adf1ad7dc30
earthkcc147 <145793741+earthkcc147@users.noreply.github.com>
2025-01-10 12:10:10 +0700

    Update function

---
+++ function/colour.py
@@ -0,0 +1,17 @@
+from colorama import Fore, Style
+
+# ฟังก์ชันกำหนดสีสำหรับข้อความต่างๆ
+def print_header(text):
+    print(Fore.YELLOW + Style.BRIGHT + "\033[1m" + text + "\033[0m")
+
+def print_menu(text):
+    print(Fore.CYAN + Style.BRIGHT + text)
+
+def print_alarm(text):
+    print(Fore.YELLOW + Style.BRIGHT + text)
+
+def print_error(text):
+    print(Fore.RED + Style.BRIGHT + text)
+
+def print_other(text):
+    print(Fore.GREEN + text)
