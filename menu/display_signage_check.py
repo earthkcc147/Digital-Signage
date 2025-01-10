f8b8d5014814f821f74fb9b774d4f29375fc40ec
earthkcc147 <145793741+earthkcc147@users.noreply.github.com>
2025-01-10 12:38:25 +0700

    Update display_signage_check.py

--- menu/display_signage_check.py
+++ menu/display_signage_check.py
@@ -1,8 +1,7 @@
-from function import initialize_file, list_files_in_folder, select_file, add_data, display_data, delete_data, edit_data, search_data, explain_program
+from function import *
 import os
 import json
 import datetime
-from function import print_header, print_menu, print_alarm, print_error
 from colorama import init, Fore, Style, Back
 
 # ฟังก์ชันหลัก
