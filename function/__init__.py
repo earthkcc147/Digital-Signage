c07a65a9de843bd01b8e65e7e47205a0ed1111c4
earthkcc147 <145793741+earthkcc147@users.noreply.github.com>
2025-01-10 09:12:10 +0700

    Update __init__.py

--- function/__init__.py
+++ function/__init__.py
@@ -1,11 +1,1 @@
-from .initialize_file import initialize_file
-from .list_files_in_folder import list_files_in_folder
-from .select_file import select_file
-from .add_data2 import add_data
-from .display_data2 import display_data
-from .delete_data2 import delete_data
-from .edit_data2 import edit_data
-from .search_data2 import search_data
-
-
-from .all2 import initialize_file, list_files_in_folder, select_file, add_data
+from .all2 import initialize_file, list_files_in_folder, select_file, add_data, display_data, delete_data, edit_data, search_data
