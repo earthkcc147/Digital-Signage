71ffe01f5bc3d45737233254e608dea7cb8415b5
earthkcc147 <145793741+earthkcc147@users.noreply.github.com>
2025-01-10 08:56:26 +0700

    Update function

---
+++ function/all_2.py
@@ -0,0 +1,20 @@
+FOLDER_NAME = 'check'
+
+
+# ฟังก์ชันตรวจสอบและสร้างโฟลเดอร์และไฟล์ถ้ายังไม่มี
+def initialize_file(filename):
+    # ตรวจสอบและสร้างโฟลเดอร์ถ้ายังไม่มี
+    if not os.path.exists(FOLDER_NAME):
+        os.makedirs(FOLDER_NAME)
+        print(f"สร้างโฟลเดอร์ {FOLDER_NAME} เรียบร้อยแล้ว")
+
+    # สร้างไฟล์ในโฟลเดอร์ถ้ายังไม่มี
+    file_path = os.path.join(FOLDER_NAME, filename)
+    if not os.path.exists(file_path):
+        with open(file_path, 'w', encoding='utf-8') as file:
+            json.dump([], file, ensure_ascii=False, indent=4)  # สร้างไฟล์พร้อมข้อมูลว่าง
+        print(f"สร้างไฟล์ {file_path} เรียบร้อยแล้ว")
+    else:
+        print(f"ไฟล์ {file_path} มีอยู่แล้ว ไม่ต้องสร้างใหม่")
+
+    return file_path
