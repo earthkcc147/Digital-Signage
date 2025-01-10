eb537348f356a0aecdab979fec69c522b046e4e6
earthkcc147 <145793741+earthkcc147@users.noreply.github.com>
2025-01-10 11:51:50 +0700

    Update all_2.py

--- function/all_2.py
+++ function/all_2.py
@@ -26,6 +26,30 @@ def initialize_file(filename):
 
 
 
+
+
+# ฟังก์ชันแสดงรายชื่อไฟล์ในโฟลเดอร์ check
+def list_files_in_folder():
+    # ตรวจสอบและสร้างโฟลเดอร์ 'check' ถ้ายังไม่มี
+    if not os.path.exists(FOLDER_NAME):
+        os.makedirs(FOLDER_NAME)
+        print(f"สร้างโฟลเดอร์ {FOLDER_NAME} เรียบร้อยแล้ว")
+
+    print("\n--- ไฟล์ที่มีอยู่ในโฟลเดอร์ 'check' ---")
+    files = os.listdir(FOLDER_NAME)
+    if files:
+        for idx, file in enumerate(files, 1):
+            print(f"{idx}. {file}")
+    else:
+        print("ยังไม่มีไฟล์ในโฟลเดอร์")
+
+
+
+
+
+
+
+
 # ฟังก์ชันให้ผู้ใช้เลือกไฟล์
 def select_file():
     try:
@@ -75,20 +99,6 @@ def select_file():
 
 
 
-# ฟังก์ชันแสดงรายชื่อไฟล์ในโฟลเดอร์ check
-def list_files_in_folder():
-    # ตรวจสอบและสร้างโฟลเดอร์ 'check' ถ้ายังไม่มี
-    if not os.path.exists(FOLDER_NAME):
-        os.makedirs(FOLDER_NAME)
-        print(f"สร้างโฟลเดอร์ {FOLDER_NAME} เรียบร้อยแล้ว")
-
-    print("\n--- ไฟล์ที่มีอยู่ในโฟลเดอร์ 'check' ---")
-    files = os.listdir(FOLDER_NAME)
-    if files:
-        for idx, file in enumerate(files, 1):
-            print(f"{idx}. {file}")
-    else:
-        print("ยังไม่มีไฟล์ในโฟลเดอร์")
 
 
 
