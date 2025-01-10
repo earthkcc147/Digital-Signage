8b6793b610a8fb7b803e20f00478cc176818db78
earthkcc147 <145793741+earthkcc147@users.noreply.github.com>
2025-01-10 12:44:03 +0700

    Update all_2.py

--- function/all_2.py
+++ function/all_2.py
@@ -1,6 +1,14 @@
 import os
 import json
 import datetime
+
+# from function.colour import (
+    print_header, 
+    print_menu, 
+    print_alarm, 
+    print_error, 
+    print_other
+)
 from function.colour import *
 
 
